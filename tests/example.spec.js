092df1da446c92271b2d091baf7f15568d43069f
Abdullah Al Noman <nomainna@gmail.com>
2026-08-29 23:12:15 -0400

    playwright test- headless false added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,9 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-
+test.use({ headless: false });
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+  
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
