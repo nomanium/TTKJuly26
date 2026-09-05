a8b31fbe79c3fc7f4476ad6c01f99ae58cc9b1a8
Abdullah Al Noman <nomainna@gmail.com>
2026-09-05 11:35:36 -0400

    Foor loops homework only. Will commit further with updates

---
+++ homework/loops.js
@@ -0,0 +1,24 @@
+// For loops homework starts
+
+//print 1-50
+
+// let i;
+// for (i = 1; i < 51; i++){
+// console.log(i);
+// }
+
+//print even numbers only
+
+// let i;
+// for (i = 2; i < 51; i+=2){
+// console.log(i);
+// }
+
+//print odds numbers only
+
+let i;
+for (i = 1; i < 51; i+=2){
+console.log(i);
+}
+
+// For loops homework ends
